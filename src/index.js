fec28404067536959728a01857daef5c3f48329d
Alka Marco (CI/DAD4.3) <alk1sgm@bosch.com>
2018-07-18 15:52:16 +0200

    import new module and build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import '../cache/css.js';
 import './modules/chat/index.js';
+import './modules/fix-style.css';
 import './modules/no-ads.js';
 import './modules/preciseNumbers.js';
 import './modules/remove-bloat.css';
